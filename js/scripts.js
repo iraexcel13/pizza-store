62d297f13159ced6404d1bb199f4360c4adcce26
iraexcel13 <iraexcel13@gmail.com>
2017-07-20 21:19:04 -0700

    Add files via upload

--- js/scripts.js
+++ js/scripts.js
@@ -1,3 +1,51 @@
+<<<<<<< HEAD
+function Pizza(){
+  this.pizzaSize = "";
+  this.pizzaTopping = [];
+  this.pizzaCost = 0;
+};
+
+Pizza.prototype.sizeCost = function() {
+
+  if (this.pizzaSize === "SMALL") {
+    this.pizzaCost= 10;
+  } else if (this.pizzaSize === "MEDIUM") {
+    this.pizzaCost = 12;
+  } else if (this.pizzaSize === "LARGE") {
+    this.pizzaCost = 15;
+  }
+  this.pizzaCost += this.pizzaTopping.length;
+  }
+
+$(document).ready(function(){
+  var pizzaInstance = new Pizza();
+
+  $("form#pizza-order").submit(function(event){
+  event.preventDefault();
+
+  $("#topping-choices").show();
+  $("input:checkbox[name=topping]:checked").each(function() {
+    var toppings = $(this).val();
+    pizzaInstance.pizzaTopping.push(toppings)
+  });
+
+  $("#size-choice").show();
+  $("input:radio[name=pizza-size]:checked").each(function() {
+    var size1 = $(this).val();
+    pizzaInstance.pizzaSize=size1;
+  });
+
+  for (var i = 0; i < pizzaInstance.pizzaTopping.length; i++){
+    $("#topping-choices").append(pizzaInstance.pizzaTopping[i] + "<br>");
+  }
+    $("#size-choice").append(pizzaInstance.pizzaSize + "<br>");
+    $("#total-cost").show();
+    pizzaInstance.sizeCost();
+    $("#total-cost").append(pizzaInstance.pizzaCost);
+    $("#pizza-order").hide();
+});
+});
+=======
 function Pizza(){
   this.pizzaSize = [];
   this.pizzaTopping = [];
@@ -49,3 +97,4 @@ $(document).ready(function(){
   $("#pizza-order").hide();
 });
 });
+>>>>>>> c5d1fb11e874350804ce465673a9716e9e385cae
